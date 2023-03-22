94565741a7ff1fba6024c4ae4fee6b380e53970c
whj768702 <whj768702@gmail.com>
2023-03-22 11:29:57 +0800

    feat(svg-react): delete icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <h1 className="text-3xl font-bold underline">
@@ -13,4 +11,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
