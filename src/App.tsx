7e248a0dea3c1ce2667a1b2eeb779c1bd859a8f2
whj768702 <whj768702@gmail.com>
2023-03-22 18:49:41 +0800

    refactor(svg-react): 删除注释代码

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,9 @@ import { DndContext } from '@dnd-kit/core';
 import type { DragEndEvent } from '@dnd-kit/core';
 
 import Draggable from './components/Draggable';
-import Droppable from './components/Droppable';
 import { drawLine } from './svgUtils/DrawLine';
 
 function App() {
-  const [isDropped, setIsDropped] = useState(false);
-
   const [currentLineId, setCurrentLineId] = useState<string | null>('');
   const [currentLinePosition, setCurrentLinePosition] = useState({ x: 0, y: 0 });
   const [lines, setLines] = useState<string[]>([]);
@@ -17,23 +14,14 @@ function App() {
   const [{ x, y }, setCoordinates] = useState({ x: 0, y: 0 });
   const [{ x: x2, y: y2 }, setCoordinates2] = useState({ x: 0, y: 0 });
 
-  // const draggableMarkup = (
-  //   <Draggable id="draggable" top={y} left={x} >Drag me 0</Draggable>
-  // );
-
   function handleDragEnd(event: DragEndEvent) {
-    console.log('event: ', event);
     const { delta, active } = event;
     if (active.id === 'draggable-1') {
-      // setCurrentLineId(active.id);
       setLines([...lines, active.id]);
       setCoordinates(({ x, y }) => ({ x: x + delta.x, y: y + delta.y }));
     } else {
       setCoordinates2(({ x, y }) => ({ x: x + delta.x, y: y + delta.y }));
     }
-    // if (event.over && event.over.id === 'Droppable') {
-    //   setIsDropped(true);
-    // }
   }
 
   function initSvg() {
@@ -47,7 +35,6 @@ function App() {
   }, []);
 
   useEffect(() => {
-    console.log('currentLineId: ', currentLineId);
     function mouseMove(event: MouseEvent) {
       if (currentLineId) {
         const line1 = document.getElementById(currentLineId);
@@ -64,7 +51,6 @@ function App() {
     }
     window.addEventListener('mousemove', mouseMove);
     return () => {
-      console.log('remove mousemove');
       window.removeEventListener('mousemove', mouseMove);
     };
   }, [currentLineId]);
@@ -73,15 +59,8 @@ function App() {
     <div className='relative'>
       <svg className='w-screen h-screen absolute'></svg>
       <DndContext onDragEnd={handleDragEnd}>
-        {/* {!isDropped ? draggableMarkup : null} */}
         <Draggable id="draggable-1" top={y} left={x} activeLineId={setCurrentLineId} updatePosition={setCurrentLinePosition} >Drag me 1</Draggable>
         <Draggable id="draggable-2" top={y2} left={x2} activeLineId={setCurrentLineId} updatePosition={setCurrentLinePosition}>Drag me 2</Draggable>
-        {/* <Draggable id="draggable-1">Drag me 1</Draggable>
-      <Draggable id="draggable-2">Drag me 2</Draggable>
-      <Draggable id="draggable-3">Drag me 3</Draggable> */}
-        {/* <Droppable>
-        {isDropped ? draggableMarkup : 'Drop here'}
-      </Droppable> */}
       </DndContext>
     </div>
   );
