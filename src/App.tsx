47b575b68e7223c46a88b35d76390c8c7fb0f45f
whj768702 <whj768702@gmail.com>
2023-03-23 17:11:23 +0800

    feat(svg-react): 连线改为贝塞尔曲线

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { DndContext } from '@dnd-kit/core';
 import type { DragEndEvent } from '@dnd-kit/core';
 
 import Draggable from './components/Draggable';
-import { drawLine } from './svgUtils/DrawLine';
+import { drawLine, drawBezierCurve } from './svgUtils/DrawLine';
 
 function App() {
   const [currentLineId, setCurrentLineId] = useState<string | null>('');
@@ -26,7 +26,7 @@ function App() {
 
   function initSvg() {
     const svg = document.querySelector('svg');
-    const line1 = drawLine(10, 10, 100, 100);
+    const line1 = drawBezierCurve(10, 15, 100, 150);
     line1.setAttribute('id', 'line1');
     svg?.appendChild(line1);
   }
@@ -34,16 +34,50 @@ function App() {
     initSvg();
   }, []);
 
+  function axisX(x1: number, x2: number) {
+    let result = 0;
+    if (x2 > x1) {
+      result = 10;
+    } else if (x2 < x1) {
+      result = -10;
+    }
+    return result;
+  }
+
+  function getOffset(x1: number, x2: number) {
+    return (x1 - x2) / 10;
+  }
+
+  function getYOffset(y1: number, y2: number) {
+    return (y1 - y2) / 10;
+  }
+
   useEffect(() => {
     function mouseMove(event: MouseEvent) {
       if (currentLineId) {
-        const line1 = document.getElementById(currentLineId);
-        if (line1?.getAttribute('x1')) {
-          line1?.setAttribute('x2', event.clientX.toString());
-          line1?.setAttribute('y2', event.clientY.toString());
+        const svg = document.getElementsByTagName('svg')[0];
+        const g = svg.querySelector(`#${currentLineId}`);
+        const line1 = g?.querySelector('path');
+        if (line1) {
+          const { x, y } = (line1 as SVGPathElement).getPointAtLength(10);
+          const x2 = event.clientX;
+          const y2 = event.clientY;
+          const xoffset = getOffset(x2, currentLinePosition.x);
+          const yoffset = getYOffset(y2, currentLinePosition.y);
+          console.log('起始点: (%s, %s)', currentLinePosition.x, currentLinePosition.y);
+          console.log('xoffset', xoffset, 'yoffset', yoffset);
+          console.log('终点: (%s, %s)', x2, y2);
+          line1.setAttribute("d", `M ${currentLinePosition.x} ${currentLinePosition.y}
+           C ${currentLinePosition.x + 3 * xoffset} ${currentLinePosition.y}
+             ${currentLinePosition.x + 4 * xoffset} ${currentLinePosition.y + 3 * yoffset}
+             ${currentLinePosition.x + 5 * xoffset} ${currentLinePosition.y + 5 * yoffset}
+           C ${currentLinePosition.x + 6 * xoffset} ${currentLinePosition.y + 7 * yoffset}
+             ${currentLinePosition.x + 7 * xoffset} ${currentLinePosition.y + 10 * yoffset}
+             ${x2} ${y2},
+             `);
         } else {
           const svg = document.querySelector('svg');
-          const line1 = drawLine(currentLinePosition.x, currentLinePosition.y, event.clientX, event.clientY);
+          const line1 = drawBezierCurve(currentLinePosition.x, currentLinePosition.y, event.clientX, event.clientY);
           line1.setAttribute('id', currentLineId);
           svg?.appendChild(line1);
         }
--- src/svgUtils/DrawLine.ts
+++ src/svgUtils/DrawLine.ts
@@ -8,4 +8,15 @@ function drawLine(x1: number, y1: number, x2: number, y2: number): SVGLineElemen
   return line;
 }
 
-export { drawLine };
+function drawBezierCurve(x1: number, y1: number, x2: number, y2: number): SVGGElement {
+  const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  path.setAttribute("d", `M ${x1} ${y1} C ${x1} ${y1} ${x2} ${y2} ${x2} ${y2}`);
+  const g = document.createElementNS("http://www.w3.org/2000/svg", "g");
+  // path.setAttribute('d', "M336.6479187011719,310.8268127441406 C358.4467315673828,310.8268127441406 358.4467315673828,228.64488220214844 380.24554443359375,228.64488220214844");
+  path.setAttribute('fill', 'none');
+  g.appendChild(path);
+  path.setAttribute("stroke", "rgb(255,0,0)");
+  return g;
+}
+
+export { drawLine, drawBezierCurve };
